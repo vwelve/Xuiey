ecb9b1567635eeb6fbd80c1bbe7464032c9d5454
Dominic Humber <dominichumber@Dominics-MacBook-Air.local>
2022-01-04 17:16:52 -0500

    Create config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+module.exports = {
+    DISCORD_TOKEN: process.env.DISCORD_TOKEN,
+    SELLIX_TOKEN: process.env.SELLIX_TOKEN,
+    CLIENT_ID: process.env.CLIENT_ID,
+    GUILD_ID: process.env.GUILD_ID
+}
